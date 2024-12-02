43f69caa604bcc64b68598c6cf392cc50c4a8091
SellamiAhmed <ahmedsellami375@gmail.com>
2024-12-02 11:33:26 +0100

    first commit

---
+++ AIQuiz/app/build/generated/source/buildConfig/release/com/sagarkhurana/quizforfun/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sagarkhurana.quizforfun;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.sagarkhurana.quizforfun";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
